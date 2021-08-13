30a5e5c26737d4cb0a9adb521a53f41b06264a98
Stephen Carroll-Keene <stephen.carrollkeene@gmail.com>
2021-08-12 18:32:43 -0600

    stylized & formatted intro/research sections

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    
+    $(".toggle").click(function() {
+
+        $(this).find(".toggleContainer").toggleClass("active");
+        $(this).siblings(".toggle").find(".toggleContainer").removeClass("active");
+    });
+
+    $(".searchButton").click(function() {
+        $(".searchButton").toggleClass("active");
+
+        if ($(".searchButton").hasClass("active")) {
+            $(".searchBar").css("height", "100vh");
+            $("#searchForm").css("opacity", "1");
+            $(".searchIcon").removeClass("fa-search").addClass("fa-times");
+        }
+        else {
+            $(".searchBar").css("height", "0vh");
+            $(".searchIcon").removeClass("fa-times").addClass("fa-search");
+        }
+    });
+
+    // fade preloader
+    function fade() {
+        $(".preloader").fadeOut("slow");
+    }
+
+    // calls fade function
+    setTimeout(fade, 3000);
+});
